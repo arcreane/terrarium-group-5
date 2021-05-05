ef076875bd3a5a9904c1f63293a18f1c0bb81db5
saly-mayu <80256863+saly-mayu@users.noreply.github.com>
2021-05-05 13:06:22 +0200

    Delete myapp/Autoterra directory

--- myapp/Autoterra/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class Humidite(forms.Form):
-    Humidite_min = forms.IntegerField(label='Humidite minimale',min_value=0)
-    Humidite_max = forms.IntegerField(label='Humidite maximale',max_value=100)
--- myapp/Autoterra/json_read.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('data.json') as json_data:
-    data_dict = json.load(json_data)
-    #json_data.get("data")
-    print(data_dict["data"])
